9be6399bc621b4cdb4bc0f26d42cdc3a0f457d8f
CarloAC <cacristofanini@gmail.com>
2018-10-02 15:14:01 +0200

    Adding new files and some modding to the 'main'

--- rockScissorsPaper.py
+++ rockScissorsPaper.py
@@ -5,7 +5,7 @@ def getFromPlayer():
     valid_input = False
     while not valid_input:
         try:
-            picked = int(print("OK! pick one between 'em:\n\t1. Rock\n\t2. Scissors\n\t3. Paper\n\n\t"))
+            picked = int(input("OK! pick one between 'em:\n\t1. Rock\n\t2. Scissors\n\t3. Paper\n\n\t"))
             valid_input = True
         except ValueError:
             print("Invalid input. Try again")
@@ -27,11 +27,34 @@ def getElement(value_picked):
 def getFromPC():
     return random.randint(1, 3)
 
+def getWinner():
+    player_value = getFromPlayer()
+    pc_value = getFromPC()
+    tmp_value = player_value - pc_value;
+    printPlayer(player_value)
+    printPC(pc_value)
+    if(tmp_value == 0):
+        return "DRAW"
+    elif((tmp_value == -1) or (tmp_value == 2)):
+        return "PLAYER WINs"
+    elif((tmp_value == 1) or (tmp_value == -2)):
+        return "PC WINs"
+    else:
+        return "HOW THE HELL DO YOU GET THERE???"
+
+def printPlayer(value):
+    print("[Player]: ")
+    print(getElement(value))
+
+def printPC(value):
+    print("[PC]: ")
+    print(getElement(value))
+
+
 def primary():
     print("Starting the game Rock Scissors Paper...")
     time.sleep(1)   #PY will sleep for 1 sec
-
-
+    print(getWinner())
 
 
 
